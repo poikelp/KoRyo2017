34c7fc11f522c83480a6a9e6f9755cbfe95af0c3
poikelp <takukelp@gmail.com>
2017-10-19 05:37:13 +0900

    vuforia

---
+++ Assets/Scripts/hoge.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoge : MonoBehaviour {
+
+	GameObject aoi;
+	GameObject mana;
+
+	Transform aoiTr;
+	Transform manaTr;
+
+	bool isManaFront = true;
+	bool wasManaFront = true;
+
+	// Use this for initialization
+	void Start () {
+		aoi = GameObject.Find ("ImageTarget(1)");
+		mana = GameObject.Find ("ImageTarget");
+
+		aoiTr = aoi.transform;
+		manaTr = mana.transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isManaFront = manaTr.position.z < aoiTr.position.z;
+		if (isManaFront != wasManaFront) {
+			//攻撃
+		}
+		wasManaFront = isManaFront;
+	}
+}
